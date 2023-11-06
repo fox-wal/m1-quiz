e475cbd2ff0c34e563d09a64c1469eddd0ac7629
ac1dTigR <artypaws7@gmail.com>
2023-11-06 21:28:51 +0000

    feat: wrote code to display results and save score to file
    change score format

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from io import TextIOWrapper
 import json as json
 from random import shuffle
+import datetime as dt
 
 def abend(message):
 	print("Program abended:", message)
@@ -14,9 +15,9 @@ def abend(message):
 CONFIG_FILE_PATH = "config.json"
 
 settings = {
-	"multiple choice" : int
-	,"number of questions" : bool
-	,"number of attempts" : InterruptedError
+	"multiple choice" : bool
+	,"number of questions" : int
+	,"number of attempts" : int
 	,"multi-choice: select using index" : bool
 }
 
@@ -43,6 +44,10 @@ else:
 			settings[setting] = value
 	
 	print("Config file loaded correctly.")
+
+# Ensure numbers of attempts and questions are at least 1.
+settings["number of attempts"] = max(1, settings["number of attempts"])
+settings["number of questions"] = max(1, settings["number of questions"])
 
 ## Question file
 
@@ -111,7 +116,7 @@ print("""
 	QQQQ Q  QQQQ UUUU    UUUU     IIII        ZZZZ
 	QQQQ  Q QQQQ UUUU    UUUU     IIII      ZZZZ
 	QQQQQQQQQQQ  UUUUUUUUUUUU IIIIIIIIIIII ZZZZZZZZZZZZ
-	 QQQQQQQQ QQQ UUUUUUUUUU  IIIIIIIIIIII ZZZZZZZZZZZZ
+	 QQQQQQQQ QQ  UUUUUUUUUU  IIIIIIIIIIII ZZZZZZZZZZZZ
 	
 	  					-----+-----
 """)
@@ -125,8 +130,9 @@ while True:
 		break
 
 # Start Quiz
-
+max_score = 0
 score = 0
+questions_correct = 0
 number_of_questions = min(len(questions), settings["number of questions"])
 for q in range(number_of_questions):
 
@@ -135,25 +141,29 @@ for q in range(number_of_questions):
 	
 	# Print answer options.
 	if settings["multiple choice"]:
+		# Ensure number of attempts does not exceed the number of options - 1.
+		settings["number of attempts"] = min(settings["number of attempts"], len(questions[q]["multi-choice options"]) - 1)
 		answer_options = questions[q]["multi-choice options"].copy() + [questions[q]["answer"]]
 		shuffle(answer_options)
 		for o in range(len(answer_options)):
-			if settings["multi-choice select using index"]:
+			if settings["multi-choice: select using index"]:
 				print("{}. {}".format(o + 1, answer_options[o]))
 			else:
 				print(answer_options[o])
 		
 		# Prompt user to choose.
-		if settings["multi-choice select using index"]:
+		if settings["multi-choice: select using index"]:
 			print("\nEnter the index of the correct answer: ")
 		else:
 			print("\nEnter the correct answer: ")
 
 	# User enters answer.
 	correct = False
+	points = settings["number of attempts"]
+	max_score += points
 	attempt = 1
 	while not correct and (attempt <= settings["number of attempts"]):
-		if settings["multiple choice"] and settings["multi-choice select using index"]:
+		if settings["multiple choice"] and settings["multi-choice: select using index"]:
 			try:
 				choice = int(input()) - 1
 				if not ((choice >= 1) and (choice <= len(answer_options))):
@@ -167,18 +177,83 @@ for q in range(number_of_questions):
 
 		if not correct:
 			print("Incorrect. {} attempts remaining.".format(settings["number of attempts"] - attempt))
+			points -= 1
 
 		attempt += 1
 	
 	if correct:
-		print("Correct! [+1]")
+		print("Correct! [+{}]".format(points))
+		score += points
+		questions_correct += 1
 	else:
 		print("Incorrect.")
 	print("Total score: {}", score)
 
 # Results
 
+print("""
++---------------End of Quiz---------------+
+
+Questions answered correctly: {} out of {}.
+Score: {} out of {}.
+
++-----------------------------------------+
+""".format(questions_correct, number_of_questions, score, max_score))
+
+# Save score
+
+## Ask if user wants to save it.
+
+options = ["Y", "N"]
+print("Would you like to save your score? [{}]".format("/".join(options)))
+
+while True:
+	choice = input().upper()
+	if choice not in options:
+		print("Please enter one of {}.".format(" or ".join(options)))
+	else:
+		break
+
+if choice == "Y":
+	time_stamp = "{:%Y-%m-%d %H:%M:%S%z}".format(dt.datetime.now())
+	adjusted_score = 100 * (score / max_score)
+	scores : dict[str, dict[str, object]] = {}
+
+	# Load score file.
+
+	SCORE_FILE_PATH = "scores.json"
+
+	try:
+		file = open(SCORE_FILE_PATH, "r+")
+	except FileNotFoundError:
+		print("Score file created.")
+	else:
+		try:
+			file_contents = json.load(file)
+		except:
+			print("Could not save score: scores file is corrupted.")
+		else:
+
+			# Write to score file.
+
+			try:
+				scores.update(dict[str, dict[str, object]](file_contents))
+			except:
+				print("Could not save score. File_contents invalid format?")
+			else:
+				if name in scores.keys():
+					scores[name][time_stamp] = adjusted_score
+				else:
+					scores[name] = {time_stamp : adjusted_score}
+				print("Score saved!")
+		finally:
+			file.close()
+	finally:
+		file = open(SCORE_FILE_PATH, "w")
+		json.dump(scores, file)
+		file.close()
 
 
-SCORE_FILE_PATH = "scores.json"
+print("Goodbye!")
+
 PROGRAM_STATE_FILE_PATH = "program_state.json"
